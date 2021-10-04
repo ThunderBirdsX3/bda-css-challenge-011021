db397547f819954d02831ed930d646895009a9cd
Supasin Dum-umpai <s.dumumpai@gmail.com>
2021-10-04 15:51:15 +0700

    Update CSS Challenge

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,10 @@
 const mix = require('laravel-mix')
 
 mix
+  .setResourceRoot('..')
   .setPublicPath('public')
   .js('src/js/app.js', 'js')
   .sass('src/scss/app.scss', 'css')
   .sass('src/scss/style.scss', 'css')
-  // .copyDirectory('resources/images', 'public/images')
+  .copyDirectory('src/images', 'public/images')
   .sourceMaps()
